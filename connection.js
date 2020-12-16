4354ae0d0aa52f0df2390b1df7b3d44cd2927337
Jimmy Wirth <jimmybean98@gmail.com>
2020-12-16 14:22:14 -0500

    hiding sensitive info test

--- connection.js
+++ connection.js
@@ -4,8 +4,17 @@ const {MongoClient} = require('mongodb');
  * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
  * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
  */
+
+username = ""
+fs = require('fs')
+username = fs.readFileSync("../personal-budget-final/username", 'utf8')
+password = ""
+password = fs.readFileSync("../personal-budget-final/password", 'utf8')
+    console.log(username)
+    console.log(password)
+
 async function main(){
-const uri = "mongodb+srv://llspeedll:fruitloop98@4166cluster.4vvux.mongodb.net/test"
+const uri = "mongodb+srv://{}username:password@4166cluster.4vvux.mongodb.net/test"
 const client = new MongoClient(uri);
 await client.connect();
 await listDatabases(client);
